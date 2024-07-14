5010d769fa1cf2e91ab9947a1b52f1698194c93e
Mustafa Schmidt <mustafa_schmidt@gmx.de>
2024-07-14 18:04:11 +0200

    Fixing error in bash file

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ sudo docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 sudo docker buildx create --name mybuilder --use
 sudo docker buildx inspect --bootstrap
 sudo docker image build . -t supermeisi/hpdirc
-sudp docker buildx build --platform linux/amd64,linux/arm64 -t supermeisi/hpdirc --push .
+sudo docker buildx build --platform linux/amd64,linux/arm64 -t supermeisi/hpdirc --push .
